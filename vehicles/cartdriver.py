c0cab035edd44d8444241feba48ab44f113cd483
landonclark97 <landonclark97@gmail.com>
2020-04-20 12:54:05 -0400

    remade cartdriver functions

--- vehicles/cartdriver.py
+++ vehicles/cartdriver.py
@@ -1,2 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
 
 
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+
+forward1 = GPIO.PWM(12, 100)
+reverse1 = GPIO.PWM(11, 100)
+
+
+def drive():
+    forward1.start(50)
+    reverse1.stop()
+
+
+def stop():
+    forward1.stop()
+    reverse1.stop()
+
+
+def turn_left():
+    reverse1.start(50)
+    forward1.stop()
+
+
+def turn_right():
+    forward1.start(50)
+    reverse1.stop()
+
+
+def shutdown():
+    forward1.stop()
+    reverse1.stop()
+    GPIO.cleanup()
